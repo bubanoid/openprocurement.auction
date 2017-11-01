f3b8842131df86a1ee1ea394da63e40d669ff8f0
godsdog <an.andrusyk@gmail.com>
2017-11-01 21:39:33 +0200

    Improve test for 'sse_timeout' function

--- openprocurement/auction/tests/test_event_source.py
+++ openprocurement/auction/tests/test_event_source.py
@@ -6,11 +6,9 @@ import openprocurement.auction.event_source as event_source_module
 SLEEP_TIME = sentinel.sleep_time
 
 
-def test_sse_timeout_without_queue(mocker):
+def test_sse_timeout(mocker):
     sleep_patch = mocker.patch.object(event_source_module, 'sleep')
-
     queue_patch = MagicMock(spec_set=Queue)
-    queue_patch.__nonzero__.return_value = False
 
     manager = MagicMock()
     manager.attach_mock(sleep_patch, 'sleep_patch')
@@ -18,26 +16,7 @@ def test_sse_timeout_without_queue(mocker):
 
     event_source_module.sse_timeout(queue_patch, SLEEP_TIME)
 
-    expected_calls = [call.sleep_patch(SLEEP_TIME),
-                      call.queue_patch.__nonzero__()]
-
-    assert manager.mock_calls == expected_calls
-
-
-def test_sse_timeout_with_queue(mocker):
-    sleep_patch = mocker.patch.object(event_source_module, 'sleep')
-
-    queue_patch = MagicMock(spec_set=Queue)
-    queue_patch.__nonzero__.return_value = True
-
-    manager = MagicMock()
-    manager.attach_mock(sleep_patch, 'sleep_patch')
-    manager.attach_mock(queue_patch, 'queue_patch')
-
-    event_source_module.sse_timeout(queue_patch, SLEEP_TIME)
-
-    expected_calls = [call.sleep_patch(SLEEP_TIME),
-                      call.queue_patch.__nonzero__(),
-                      call.queue_patch.put({"event": "StopSSE"})]
-
-    assert manager.mock_calls == expected_calls
+    assert sleep_patch.called_once_with(SLEEP_TIME)
+    assert queue_patch.put.called_once_with({"event": "StopSSE"})
+    assert manager.mock_calls.index(call.sleep_patch(SLEEP_TIME)) < \
+        manager.mock_calls.index(call.queue_patch.put({"event": "StopSSE"}))
